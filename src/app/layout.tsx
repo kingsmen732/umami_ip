bc57bcef805b03d857e6b22f57531386bd60b764
Nithin <nithinjambula89@gmail.com>
2025-08-26 20:31:07 +0530

    updated logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function ({ children }) {
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | Umami',
-    default: 'Umami',
+    template: '%s | AIR Centre',
+    default: 'AIR Centre',
   },
 };
